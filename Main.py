a0ab191b39788145e5a5e44db99946775a176889
Lukas Zeppelin <Lukas.zeppelin.ry@gmail.com>
2022-02-28 15:19:23 +0100

    added defects detector

--- Main.py
+++ Main.py
@@ -1,7 +1,6 @@
 import cv2
 import glob
 import mainClass as _
-import Functions as f
 
 path = "C:\\Users\lukas\PycharmProjects\Raman-img-homo-recog\Raman\Visible"
 
@@ -10,8 +9,16 @@ files = []
 for f in glob.glob(path+"/*"):
     files.append(cv2.resize(cv2.cvtColor(cv2.imread(f),cv2.COLOR_BGR2GRAY)/255,(420,420))) #normalizing and storing images to list
 
-obj = _.Main(files[0:100],boundaries=(25,120))
+obj = _.Main(files,boundaries=(25,120))
+
 
 # obj.average_voxels(img=10,show_img=True)
 arr = obj.extract_all_img_homogeneity(rem_outliers=True,plot=False)
 obj.min_max_idx()
+# obj.plot_defects(img=17)
+
+
+
+# vid = _.VideoFunc(files=files[10])
+# vid.extract_all_img_homogeneity_vid()
+# vid.graph2vid()
--- mainClass.py
+++ mainClass.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import numpy as np
 from scipy import stats
+import os
+import glob
 import sys
 sys.path.append("External_Functions")
 from ExternalFunctions import nice_string_output, add_text_to_ax
@@ -37,6 +39,7 @@ class Main:
         axes[0][1].plot(self.arr[idx_min][:],
                         stats.norm.pdf(self.arr[idx_min][:],np.mean(self.arr[idx_min][:]),np.std(self.arr[idx_min][:])),
                         c="r") #plotting norm pdf
+        # axes[0][1].set_xlim(0.3,0.9)
         axes[0][0].imshow(self.files[idx_min])
         axes[0][0].set_title("homoegenoust")
 
@@ -82,6 +85,7 @@ class Main:
         if show_img:
             cv2.imshow("blehh",self.files[img])
             cv2.waitKey(0)
+        self.vox_avg = vox_avg
         return vox_avg
 
     def hist_plot(self,vox_avg,idx=int):
@@ -130,10 +134,145 @@ class Main:
         self.arr=arr
         return arr
 
+    def min_max(self,lst):
+        idx_max = np.argmax(lst)
+        idx_min = np.argmin(lst)
+        return (idx_min,idx_max)
+
+    def plot_defects(self,img=int,avg_voxel_multiplier=None):
+        avg_voxels = self.average_voxels(img=img)
+        if avg_voxel_multiplier == None:
+            print("clean list")
+            idx_min,idx_max = self.min_max(lst=avg_voxels)
+        else:
+            print("averaging averages")
+            new_avg_list = []
+            for i in range(len(avg_voxels)):
+                temp = 0
+                for n in range(avg_voxel_multiplier):
+                    temp += avg_voxels[i]/avg_voxel_multiplier
+                new_avg_list.append(temp)
+            idx_min, idx_max = self.min_max(lst=new_avg_list)
+
+        fig, ax = plt.subplots()
+
+        ax.imshow(self.files[img])
+
+        rect0 = patches.Rectangle((idx_min, idx_min), 25, 25, linewidth=2, edgecolor='b', facecolor='none',label="min voxel avg.")
+        rect1 = patches.Rectangle((idx_max, idx_max), 25, 25, linewidth=2, edgecolor='r', facecolor='none',label="max voxel avg.")
+
+        ax.add_patch(rect0)
+        ax.add_patch(rect1)
+
+        plt.legend()
+
+        fig.tight_layout()
+        plt.show()
+
+class DetectDefects(Main):
+    def __init__(self,img=int,boundaries=(25,120)):
+        self.vox_avg = self.average_voxels(img=img,show_img=False)
+        self.img = self.files[img]
+        self.boundaries = boundaries
+
+    def min_max(self):
+        idx_max = np.argmax(self.vox_avg)
+        idx_min = np.argmin(self.vox_avg)
+        return (idx_min,idx_max)
+
+    def plot_defects(self,square_multiplier=3):
+        idx_min,idx_max = self.min_max()
+
+        fig, axes = plt.subplots(nrows=1,ncols=1)
+
+        axes[0].imshow(self.img)
+
+        rect0 = patches.Rectangle((idx_min, idx_min), 30, 30, linewidth=2, edgecolor='r', facecolor='none')
+        rect1 = patches.Rectangle((idx_max, idx_max), 30, 30, linewidth=2, edgecolor='r', facecolor='none')
+
+        axes[0].add_patch(rect0)
+        axes[0].add_patch(rect1)
+
+        fig.tight_layout()
+        plt.show()
+
+class VideoFunc(Main):
+    def __init__(self,files,boundaries=(25,120)):
+        self.files = files
+        self.boundaries = boundaries
+
+    def average_voxels_vid(self,img=int):
+        vox_avg = []
+        temp = []
+        for n in range(self.boundaries[0],self.boundaries[1]):
+            temp.clear()
+            for (i) in range((1+(3*(n-1))),(3*n)):
+                for j in range((1+(3*(n-1))),(3*n)):
+                    temp.append(self.files[img][i,j])
+                vox_avg.append(sum(temp)/len(temp))
+                self.hist_plot_vid(vox_avg,idx=img)
+        return vox_avg
+
+    def hist_plot_vid(self, vox_avg, idx=int):
+        vox_avg.sort()
+        if self.rem_outliers:
+            vox_avg = self.remove_outliers(vox_avg, np.std(vox_avg), np.mean(vox_avg))
+            print("Removing outliers")
+        y = stats.norm.pdf(vox_avg, np.mean(vox_avg), np.std(vox_avg))
+        plt.clf()
+        fig, axes = plt.subplots(nrows=2, ncols=1)
+
+        axes[0].hist(vox_avg, bins=30)
+
+        d = {"mean": np.mean(vox_avg),
+             "std": np.std(vox_avg)}
+
+        text = nice_string_output(d, extra_spacing=2, decimals=3)
+        add_text_to_ax(0.02, 0.97, text, axes[0], fontsize=14)
+
+        axes[0].set_xlim((np.mean(vox_avg) - 4 * np.std(vox_avg), np.mean(vox_avg) + 4 * np.std(vox_avg)))
+        axes2 = axes[0].twinx()
+        axes2.plot(vox_avg, y, c="r")
+
+        axes[1].imshow(self.files[idx])
+        rect = patches.Rectangle((self.boundaries[0], self.boundaries[0]), self.boundaries[1] * 3,
+                                 self.boundaries[1] * 3, linewidth=3, edgecolor='r', facecolor='none')
+        axes[1].add_patch(rect)
+        fig.tight_layout()
+        plt.savefig("temp/pic %s"%len(vox_avg))
+
+    def extract_all_img_homogeneity_vid(self,rem_outliers=False):
+        if rem_outliers:
+            self.rem_outliers=rem_outliers
+        else:
+            self.rem_outliers = rem_outliers
+        for i in range(len(self.files)):
+            self.average_voxels_vid(i)
 
 
+    def graph2vid(self, options='video',filename = "video",fpsrate=1, empty_dir_on_done=True): #turns a series of n pictures in directory into a video of the .avi type or a gif
+        self.img_save_dir = "temp"
+        list1 = os.listdir(self.img_save_dir)
 
+        img_array = []
 
+        size = (640,480)
 
+        if options == 'video':
+            for i in range(len(list1)):
+                img = cv2.imread(self.img_save_dir + '/pic %s.png' %(i))
+                for n in range(fpsrate):
+                    img_array.append(img)
+                    if (i + 1) % (len(list1)) == 0:
+                        for i in range(fpsrate + 3):
+                            img_array.append(img)
+            out = cv2.VideoWriter(filename + '.avi',cv2.VideoWriter_fourcc(*'DIVX'), 30, size)
 
-
+            for i in range(len(img_array)):
+                out.write(img_array[i])
+            out.release()
+            print('Video made')
+        if empty_dir_on_done == True:
+            files = glob.glob(self.img_save_dir+'/*')
+            for f in files:
+                os.remove(f)
